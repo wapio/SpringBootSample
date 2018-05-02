423d369a43be623b15ef7553ab6dfbcb789826c8
wapio <o2kbabata@gmail.com>
2018-05-02 13:45:13 +0900

    20180502

---
+++ HelloSpringBoot/src/main/java/com/wakame/wapio/HelloSpringBoot/web/form/MenuForm.java
@@ -0,0 +1,33 @@
+package com.wakame.wapio.HelloSpringBoot.web.form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+public class MenuForm implements Serializable {
+
+	@NotNull
+	private String profile;
+
+	/**
+	 * @return the profile
+	 */
+	public String getProfile() {
+		return profile;
+	}
+
+	/**
+	 * @param profile the profile to set
+	 */
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "MenuForm [profile=" + profile + "]";
+	}
+}
